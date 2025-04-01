1d51452aa079f5d12aac334414fe653a7bb462a7
workm <workmail2006@bk.ru>
2025-04-01 23:07:54 +0300

    add updated ACE_CHANGE_TOOL

--- extras/ace.py
+++ extras/ace.py
@@ -22,7 +22,7 @@ class BunnyAce:
         self.feed_speed = config.getint('feed_speed', 50)
         self.retract_speed = config.getint('retract_speed', 50)
         self.toolchange_retract_length = config.getint('toolchange_retract_length', 100)
-        self.toolhead_sensor_to_nozzle_length = config.getint('toolhead_sensor_to_nozzle', None)
+        self.toolhead_sensor_to_nozzle_length = config.getint('toolhead_sensor_to_nozzle', 0)
         # self.extruder_to_blade_length = config.getint('extruder_to_blade', None)
 
         self.max_dryer_temperature = config.getint('max_dryer_temperature', 55)
@@ -85,8 +85,8 @@ class BunnyAce:
             ]
         }
 
-        #self._create_mmu_sensor(config, extruder_sensor_pin, "extruder_sensor")
-        #self._create_mmu_sensor(config, toolhead_sensor_pin, "toolhead_sensor")
+        self._create_mmu_sensor(config, extruder_sensor_pin, "extruder_sensor")
+        self._create_mmu_sensor(config, toolhead_sensor_pin, "toolhead_sensor")
         self.printer.register_event_handler('klippy:ready', self._handle_ready)
         self.printer.register_event_handler('klippy:disconnect', self._handle_disconnect)
         self.gcode.register_command(
@@ -98,6 +98,21 @@ class BunnyAce:
         self.gcode.register_command(
             'ACE_STOP_DRYING', self.cmd_ACE_STOP_DRYING,
             desc=self.cmd_ACE_STOP_DRYING_help)
+        self.gcode.register_command(
+            'ACE_ENABLE_FEED_ASSIST', self.cmd_ACE_ENABLE_FEED_ASSIST,
+            desc=self.cmd_ACE_ENABLE_FEED_ASSIST_help)
+        self.gcode.register_command(
+            'ACE_DISABLE_FEED_ASSIST', self.cmd_ACE_DISABLE_FEED_ASSIST,
+            desc=self.cmd_ACE_DISABLE_FEED_ASSIST_help)
+        self.gcode.register_command(
+            'ACE_FEED', self.cmd_ACE_FEED,
+            desc=self.cmd_ACE_FEED_help)
+        self.gcode.register_command(
+            'ACE_RETRACT', self.cmd_ACE_RETRACT,
+            desc=self.cmd_ACE_RETRACT_help)
+        self.gcode.register_command(
+            'ACE_CHANGE_TOOL', self.cmd_ACE_CHANGE_TOOL,
+            desc=self.cmd_ACE_CHANGE_TOOL_help)
 
 
     def _calc_crc(self, buffer):
@@ -239,10 +254,44 @@ class BunnyAce:
         self._queue = None
         self._main_queue = None
 
+    def dwell(self, delay = 1.):
+        toolhead = self.printer.lookup_object('toolhead')
+        toolhead.dwell(delay)
 
     def send_request(self, request, callback):
         self._info['status'] = 'busy'
         self._queue.put([request, callback])
+
+    def wait_ace_ready(self):
+        while self._info['status'] != 'ready':
+            self.dwell(delay=0.5)
+
+    def _extruder_move(self, length, speed):
+        pos = self.toolhead.get_position()
+        pos[3] += length
+        self.toolhead.move(pos, speed)
+        return pos[3]
+
+    def _create_mmu_sensor(self, config, pin, name):
+        section = "filament_switch_sensor %s" % name
+        config.fileconfig.add_section(section)
+        config.fileconfig.set(section, "switch_pin", pin)
+        config.fileconfig.set(section, "pause_on_runout", "False")
+        fs = self.printer.load_object(config, section)
+
+        ppins = self.printer.lookup_object('pins')
+        pin_params = ppins.parse_pin(pin, True, True)
+        share_name = "%s:%s" % (pin_params['chip_name'], pin_params['pin'])
+        ppins.allow_multi_use_pin(share_name)
+        mcu_endstop = ppins.setup_pin('endstop', pin)
+
+        query_endstops = self.printer.load_object(config, "query_endstops")
+        query_endstops.register_endstop(mcu_endstop, share_name)
+        self.endstops[name] = mcu_endstop
+
+    def _check_endstop_state(self, name):
+        print_time = self.toolhead.get_last_move_time()
+        return bool(self.endstops[name].query_endstop(print_time))
 
 
     cmd_ACE_START_DRYING_help = 'Starts ACE Pro dryer'
@@ -277,6 +326,193 @@ class BunnyAce:
 
         self.send_request(request={"method": "drying_stop"}, callback=callback)
 
+    def _enable_feed_assist(self, index):
+        def callback(self, response):
+            if 'code' in response and response['code'] != 0:
+                raise ValueError("ACE Error: " + response['msg'])
+            else:
+                self._feed_assist_index = index
+                self.gcode.respond_info(str(response))
+
+        self.send_request(request={"method": "start_feed_assist", "params": {"index": index}}, callback=callback)
+        self.dwell(delay=0.7)
+
+    cmd_ACE_ENABLE_FEED_ASSIST_help = 'Enables ACE feed assist'
+
+    def cmd_ACE_ENABLE_FEED_ASSIST(self, gcmd):
+        index = gcmd.get_int('INDEX')
+
+        if index < 0 or index >= 4:
+            raise gcmd.error('Wrong index')
+
+        self._enable_feed_assist(index)
+
+    def _disable_feed_assist(self, index):
+        def callback(self, response):
+            if 'code' in response and response['code'] != 0:
+                raise ValueError("ACE Error: " + response['msg'])
+
+            self._feed_assist_index = -1
+            self.gcode.respond_info('Disabled ACE feed assist')
+
+        self.send_request(request={"method": "stop_feed_assist", "params": {"index": index}}, callback=callback)
+        self.dwell(0.3)
+
+    cmd_ACE_DISABLE_FEED_ASSIST_help = 'Disables ACE feed assist'
+
+    def cmd_ACE_DISABLE_FEED_ASSIST(self, gcmd):
+        if self._feed_assist_index != -1:
+            index = gcmd.get_int('INDEX', self._feed_assist_index)
+        else:
+            index = gcmd.get_int('INDEX')
+
+        if index < 0 or index >= 4:
+            raise gcmd.error('Wrong index')
+
+        self._disable_feed_assist(index)
+
+    def _feed(self, index, length, speed):
+        def callback(self, response):
+            if 'code' in response and response['code'] != 0:
+                raise ValueError("ACE Error: " + response['msg'])
+
+        self.send_request(
+            request={"method": "feed_filament", "params": {"index": index, "length": length, "speed": speed}},
+            callback=callback)
+        self.dwell(delay=(length / speed) + 0.1)
+
+    cmd_ACE_FEED_help = 'Feeds filament from ACE'
+
+    def cmd_ACE_FEED(self, gcmd):
+        index = gcmd.get_int('INDEX')
+        length = gcmd.get_int('LENGTH')
+        speed = gcmd.get_int('SPEED', self.feed_speed)
+
+        if index < 0 or index >= 4:
+            raise gcmd.error('Wrong index')
+        if length <= 0:
+            raise gcmd.error('Wrong length')
+        if speed <= 0:
+            raise gcmd.error('Wrong speed')
+
+        self._feed(index, length, speed)
+
+    def _retract(self, index, length, speed):
+        def callback(self, response):
+            if 'code' in response and response['code'] != 0:
+                raise ValueError("ACE Error: " + response['msg'])
+
+        self.send_request(
+            request={"method": "unwind_filament", "params": {"index": index, "length": length, "speed": speed}},
+            callback=callback)
+        self.dwell(delay=(length / speed) + 0.1)
+
+    cmd_ACE_RETRACT_help = 'Retracts filament back to ACE'
+
+    def cmd_ACE_RETRACT(self, gcmd):
+        index = gcmd.get_int('INDEX')
+        length = gcmd.get_int('LENGTH')
+        speed = gcmd.get_int('SPEED', self.retract_speed)
+
+        if index < 0 or index >= 4:
+            raise gcmd.error('Wrong index')
+        if length <= 0:
+            raise gcmd.error('Wrong length')
+        if speed <= 0:
+            raise gcmd.error('Wrong speed')
+
+        self._retract(index, length, speed)
+
+    def _park_to_toolhead(self, tool):
+
+        sensor_extruder = self.printer.lookup_object("filament_switch_sensor %s" % "extruder_sensor", None)
+
+        self.wait_ace_ready()
+
+        self._feed(tool, self.toolchange_retract_length - 5, self.retract_speed)
+        self.variables['ace_filament_pos'] = "bowden"
+
+        self.wait_ace_ready()
+
+        self._enable_feed_assist(tool)
+
+        while not bool(sensor_extruder.runout_helper.filament_present):
+            self.dwell(delay=0.1)
+
+        if not bool(sensor_extruder.runout_helper.filament_present):
+            raise ValueError("Filament stuck " + str(bool(sensor_extruder.runout_helper.filament_present)))
+        else:
+            self.variables['ace_filament_pos'] = "spliter"
+
+        while not self._check_endstop_state('toolhead_sensor'):
+            self._extruder_move(1, 5)
+
+        self.variables['ace_filament_pos'] = "toolhead"
+
+        self._extruder_move(self.toolhead_sensor_to_nozzle_length, 5)
+        self.variables['ace_filament_pos'] = "nozzle"
+
+    cmd_ACE_CHANGE_TOOL_help = 'Changes tool'
+
+    def cmd_ACE_CHANGE_TOOL(self, gcmd):
+        tool = gcmd.get_int('TOOL')
+        sensor_extruder = self.printer.lookup_object("filament_switch_sensor %s" % "extruder_sensor", None)
+
+        if tool < -1 or tool >= 4:
+            raise gcmd.error('Wrong tool')
+
+        was = self.variables.get('ace_current_index', -1)
+        if was == tool:
+            gcmd.respond_info('ACE: Not changing tool, current index already ' + str(tool))
+            return
+
+        if tool != -1:
+            status = self._info['slots'][tool]['status']
+            if status != 'ready':
+                self.gcode.run_script_from_command('_ACE_ON_EMPTY_ERROR INDEX=' + str(tool))
+                return
+        self._park_in_progress = True
+        self.gcode.run_script_from_command('_ACE_PRE_TOOLCHANGE FROM=' + str(was) + ' TO=' + str(tool))
+
+        logging.info('ACE: Toolchange ' + str(was) + ' => ' + str(tool))
+        if was != -1:
+            self._disable_feed_assist(was)
+            self.wait_ace_ready()
+            if self.variables.get('ace_filament_pos', "spliter") == "nozzle":
+                self.gcode.run_script_from_command('CUT_TIP')
+                self.variables['ace_filament_pos'] = "toolhead"
+
+            if self.variables.get('ace_filament_pos', "spliter") == "toolhead":
+                while bool(sensor_extruder.runout_helper.filament_present):
+                    self._extruder_move(-20, 10)
+                    self._retract(was, 20, self.retract_speed)
+                    self.wait_ace_ready()
+                self.variables['ace_filament_pos'] = "bowden"
+
+            self.wait_ace_ready()
+
+            self._retract(was, self.toolchange_retract_length, self.retract_speed)
+            self.wait_ace_ready()
+            self.variables['ace_filament_pos'] = "spliter"
+
+            if tool != -1:
+                self._park_to_toolhead(tool)
+        else:
+            self._park_to_toolhead(tool)
+
+        gcode_move = self.printer.lookup_object('gcode_move')
+        gcode_move.reset_last_position()
+
+        self.gcode.run_script_from_command('_ACE_POST_TOOLCHANGE FROM=' + str(was) + ' TO=' + str(tool))
+        self.variables['ace_current_index'] = tool
+        gcode_move.reset_last_position()
+        # Force save to disk
+        self.gcode.run_script_from_command('SAVE_VARIABLE VARIABLE=ace_current_index VALUE=' + str(tool))
+        self.gcode.run_script_from_command(
+            f"""SAVE_VARIABLE VARIABLE=ace_filament_pos VALUE='"{self.variables['ace_filament_pos']}"'""")
+        self._park_in_progress = False
+        gcmd.respond_info(f"Tool {tool} load")
+
     def cmd_ACE_DEBUG(self, gcmd):
         for i in range(0, 10):
             try:
@@ -301,6 +537,10 @@ class BunnyAce:
         self.writer_timer = self.reactor.register_timer(self._writer, self.reactor.NOW)
         self.reader_timer = self.reactor.register_timer(self._reader, self.reactor.NOW)
         self.send_request(request={"method": "get_info"}, callback=lambda self, response: self.gcode.respond_info(str(response)))
+
+
+    def get_status(self, eventtime=None):
+        return self._info
 
 
 
