8e67da928600d2710fd2ac1233f248e24ab18c84
srinn <37666946+srinn@users.noreply.github.com>
2025-07-18 22:55:00 +0900

    Update ace.py

--- extras/ace.py
+++ extras/ace.py
@@ -762,8 +762,8 @@ class BunnyAce:
 
             if self.save_variables.allVariables.get('ace_filament_pos', "spliter") == "toolhead":
                 while bool(sensor_extruder.runout_helper.filament_present):
-                    self._extruder_move(-30, 10)
-                    self._retract(was, 30, 13)
+                    self._extruder_move(-40, 10)
+                    self._retract(was, 40, 7)
                     self.wait_ace_ready()
                 self.save_variable('ace_filament_pos', "bowden", True)
 
