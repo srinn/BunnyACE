70ff9d577f8affbe81a92602c9dc12e61eceb8f7
srinn <37666946+srinn@users.noreply.github.com>
2025-07-21 18:41:08 +0900

    Update ace.py

--- extras/ace.py
+++ extras/ace.py
@@ -807,14 +807,14 @@ class BunnyAce:
                 self.save_variable('ace_filament_pos', "toolhead", True)
 
             if self.save_variables.allVariables.get('ace_filament_pos', "spliter") == "toolhead":
-                self._retract(was, 10, 10, 1)
+                self._retract(was, 100, 10, 1)
                 while bool(sensor_extruder.runout_helper.filament_present):
                     # self.gcode.respond_info('ACE: check extruder sensor')
                     if self._info['status'] == 'ready':
-                        self._retract(was, 10, 10, 1)
+                        self._retract(was, 100, 10, 1)
                     self._extruder_move(-5, 10)
                     self.dwell(delay=0.01)
-                self._stop_feeding(was)
+                self._stop_retracting(was)
                 self.save_variable('ace_filament_pos', "bowden", True)
 
             self.wait_ace_ready()
