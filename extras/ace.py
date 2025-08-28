b57aabf85876f2071be052552a934342e81cea1e
srinn <37666946+srinn@users.noreply.github.com>
2025-08-28 16:23:59 +0900

    fix bug

--- extras/ace.py
+++ extras/ace.py
@@ -525,7 +525,7 @@ class BunnyAce:
         self.variables['ace_filament_pos'] = 'bowden'
         self.gcode.respond_info('ACE: start feeding')
 
-        self._feed(tool, self.toolchange_retract_length + (1000 if 'toolhead_sensor' in self.endstops else 200), self.retract_speed, 1)
+        self._feed(tool, self.toolchange_retract_length + (1000 if sensor_toolhead is not None else 200), self.retract_speed, 1)
         # self._set_feeding_speed(tool, 10)
         # self._stop_feeding(tool)
         # self.wait_ace_ready()
